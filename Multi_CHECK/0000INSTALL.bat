b92b55d3a33fa8937afee452174010044453f577
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2022-05-23 23:31:40 +0300

    Update 0000INSTALL.bat

--- Multi_CHECK/0000INSTALL.bat
+++ Multi_CHECK/0000INSTALL.bat
@@ -1,9 +1,8 @@
 @Echo off
 title Multi TRX Attacker [MMDRZA.CoM]
 Pushd "%~dp0"
+python -m pip install --upgrade pip
 pip install rich
 pip install tropapi
 pip install colorama
-:loop
 python AttackxTRX_V3.py
-goto loop
