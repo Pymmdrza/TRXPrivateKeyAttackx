ed6a290fa24af77b93347aa7d43c15f111efbef9
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2022-05-23 22:37:13 +0300

    After Install Running For Batch Finder
    After Install Running For Batch Finder

---
+++ Multi_CHECK/Attacker.cmd
@@ -0,0 +1,19 @@
+@Echo off
+title Multi TRX Attacker [MMDRZA.CoM]
+Pushd "%~dp0"
+@Echo off
+title dogev2.py
+Pushd "%~dp0"
+start X1.cmd
+start X2.cmd
+start X3.cmd
+start X4.cmd
+start X5.cmd
+start X6.cmd
+start X7.cmd
+start X8.cmd
+start X9.cmd
+start X10.cmd
+:loop
+python AttackxTRX_V3.py
+goto loop
